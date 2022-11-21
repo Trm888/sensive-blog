18a20652c3efb6ad50683cdb0ae2262487fb2043
Trm888 <bigtram888@gmail.com>
2022-11-21 20:42:37 +0900

    Optimize comments count evaluation.

--- blog/views.py
+++ blog/views.py
@@ -29,7 +29,7 @@ def serialize_post_optimized(post):
         'title': post.title,
         'teaser_text': post.text[:200],
         'author': post.author.username,
-        'comments_amount': post.comments__count,
+        'comments_amount': post.comments_count,
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
@@ -45,11 +45,25 @@ def serialize_tag(tag):
 
 
 def index(request):
-    posts = Post.objects.annotate(Count('likes', distinct=True), Count('comments', distinct=True)).prefetch_related('author')
+    # posts = Post.objects.annotate(Count('likes', distinct=True), Count('comments', distinct=True))\
+    #     .prefetch_related('author')
+    #
+    # most_popular_posts = posts.order_by('-likes__count')[:5]
 
-    most_popular_posts = posts.order_by('-likes__count')[:5]
+    posts = Post.objects.annotate(likes_count=Count('likes')).order_by('-likes_count').prefetch_related('author')
+    most_popular_posts = posts[:5]
+    most_popular_posts_ids = [post.id for post in most_popular_posts]
+    posts_with_comments = Post.objects.filter(id__in=most_popular_posts_ids).annotate(comments_count=Count('comments'))
+    ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
+    count_for_id = dict(ids_and_comments)
+    for post in most_popular_posts:
+        post.comments_count = count_for_id[post.id]
 
-    most_fresh_posts = posts.order_by('-published_at')[:5]
+
+
+    most_fresh_posts = Post.objects.annotate(comments_count=Count('comments')).order_by('-published_at')[:5]\
+        .prefetch_related('author')
+        # posts.order_by('-published_at')[:5]
 
 
     most_popular_tags = Tag.objects.annotate(Count('posts')).order_by('-posts__count')[:5]
