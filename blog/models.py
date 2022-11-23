894bf604eae5c647d692458f3df108248c6d78b5
Trm888 <bigtram888@gmail.com>
2022-11-23 23:33:41 +0900

    Edit name function.

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 
 class PostQuerySet(models.QuerySet):
 
-    def year(self, year):
+    def get_posts_at_year(self, year):
         posts_at_year = self.filter(published_at__year=year).order_by('published_at')
         return posts_at_year
 
